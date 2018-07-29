06a165e0aec8c2ea81a1a919beef582804c0d8d6
ennab <40575668+ennab@users.noreply.github.com>
2018-07-29 15:35:13 -0500

    finish section 3

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
@@ -9,6 +7,15 @@ namespace Vidly.Models
     {
         //Movie
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
     }
 }
