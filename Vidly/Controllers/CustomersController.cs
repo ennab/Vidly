65f5ab21a9d574831825283f1f4d324c4e6115fe
ennab <40575668+ennab@users.noreply.github.com>
2018-07-29 20:07:23 -0500

    Update Customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -21,14 +21,37 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel { MembershipTypes = membershipTypes };
-            return View(viewModel);
+            var viewModel = new CustomerFormViewModel { MembershipTypes = membershipTypes };
+            return View("CustomerForm", viewModel);
         }
         [HttpPost]
-        public ActionResult Create(NewCustomerViewModel viewModel)
+        public ActionResult Save(Customer customer)
         {
+            if (customer.Id == 0) { _context.Customers.Add(customer); }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.DateofBirth = customer.DateofBirth;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
+            _context.SaveChanges();
 
-            return View();
+            return RedirectToAction("Index", "Customers");
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null) { return HttpNotFound(); }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
         public ViewResult Index()
         {
